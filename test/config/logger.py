151ed6fca6dd414943908f802a4aafa6eadb7f19
leviraphael <levi.raphael@gmail.com>
2023-07-02 20:07:15 +0300

    Update logger.py

--- test/config/logger.py
+++ test/config/logger.py
@@ -2,9 +2,7 @@ import logging
 import logging.config
 import os
 
-logging.config.fileConfig(os.path.join(os.path.dirname(__file__), 'logging_config.ini'))
-
-logging.info('This is an informational message')
+logging.config.fileConfig(os.path.join(os.path.dirname(__file__), "logging_config.ini"))
 
 
 def get_logger():
